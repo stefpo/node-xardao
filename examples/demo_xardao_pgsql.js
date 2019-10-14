651d0cc399367de8f3d86f96d513c06b72d47d72
Stephane Potelle <stephane.potelle@gmail.com>
2019-10-14 18:08:33 +0200

    Added PostgreSQL support

--- examples/demo_xardao_pgsql.js
+++ examples/demo_xardao_pgsql.js
@@ -7,7 +7,7 @@
 ********************************************************************************/
 rdao = require ('../lib/xardao.js'); 
 
-cn = new rdao.Connection('sqlite');
+cn = new rdao.Connection('pg');
 
 function logError(e) {
     console.log('Error '+ e)
@@ -16,21 +16,22 @@ function logError(e) {
 function test1(next) {
 
     test_openConn();
-    
+
+  
     function test_openConn() {
-        cn.open('test_database.sqlite', 
+        cn.open({ host: 'localhost', user: 'apptestusr', password: 'apptestpw', database: 'apptest'}, 
             function (err) { 
-                if (err) { logError(err); }
-                else { console.log('SQLite Database opened'); test_exec() }
+                if (err) { logError(err); closeDatabase() }
+                else { console.log('Database opened'); test_exec(); }
                 },
         );
     }
 
     function test_exec() {
-        let insertSql= "insert into contact (Firstname, Lastname, Birthdate, Age) values (@Firstname, @Lastname, @Birthdate, @Age)"
+        let insertSql= "insert into contact (Firstname, Lastname, Birthdate, Age) values (@Firstname, @Lastname, @Birthdate, @Age) returning id"
         let script =  [];
         //cn.debugMode = false;
-        script.push("begin transaction");
+        script.push("start transaction");
         script.push("drop table if exists contact");
         script.push("create table contact( Id serial primary key, Firstname varchar(50), Lastname varchar(50), Birthdate timestamp, age int)");
         script.push({ sql: insertSql,
@@ -49,7 +50,7 @@ function test1(next) {
                             Age: 18 }                    
                         });
         }
-        script.push("commit transaction");
+        script.push("commit");
         cn.exec(script, 
             function(err) {
                 if (err) closeDatabase();
@@ -57,40 +58,11 @@ function test1(next) {
             } );
     }
 
-    function test_execX() {
-        let insertSql= "insert into contact (Firstname, Lastname, Birthdate, Age) values (@Firstname, @Lastname, @Birthdate, @Age)"
-        cn.batch()
-            .add("begin transaction")
-            .add("drop table if exists contact")
-            .add("create table contact( Id integer primary key autoincrement, Firstname text, Lastname text, Birthdate datetime, age int)")
-            .add({ sql: insertSql,
-                    params: {
-                        Firstname: 'James', 
-                        Lastname: 'O\'Connor', 
-                        Birthdate: new Date(1957,7,9), 
-                        Age: 62 }
-                    })
-                    /*
-        for (let i=0; i<10; i++) {                     
-            script.push({ sql: insertSql,
-                        params: {
-                            Firstname: 'John'+i, 
-                            Lastname: 'Doe-'+i, 
-                            Birthdate: new Date(2001,5,8), 
-                            Age: 18 }                    
-                        })
-        }*/
-            .add("commit transaction")
-            .exec (  function(err) {
-                if (err) closeDatabase();
-                else test_getDataTable();
-            } );  
-
-    }    
-
     function test_getDataTable() {
+        //cn.timeoutMilliSeconds = 1;
+
         cn.getDataTable("select * from contact",
-            function(err, dt) {
+            function(err,dt) {
                 if (err) closeDatabase();
                 else { console.log( dt.json());  test_getScalar(); }
             } ); 
@@ -98,7 +70,7 @@ function test1(next) {
 
     function test_getScalar() {
         cn.getScalar( { sql:"select age from contact where Firstname=@Firstname", params: { 'Firstname': 'James' } },
-            function(err, v) {
+            function(err,v) {
                 if (err) closeDatabase();
                 else {console.log(v); test_getlist();} 
             } );             
@@ -106,7 +78,7 @@ function test1(next) {
 
     function test_getlist(){
         cn.getKVList( "select Firstname, Lastname from contact" ,    
-            function(err, l) {
+            function(err,v) {
                 if (err) closeDatabase();
                 else {console.log(JSON.stringify(l)); closeDatabase();}
             } );                
